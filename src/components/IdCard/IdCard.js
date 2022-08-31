8c891a52d81a83098f4192954dd3d12d9cf65806
Lucas Vitro <lucasvitro9@gmail.com>
2022-08-30 22:26:19 -0400

    interaction 2

---
+++ src/components/IdCard/IdCard.js
@@ -0,0 +1,31 @@
+import style from "./IdCard.module.css"
+
+export function IdCard(props) {
+    return (
+      <div className={style.card}>
+        <img src={props.picture} alt="User" />
+        <div className={style.content}>
+          <p>
+            <strong>First name:</strong>
+            <span>{props.firstName}</span>
+          </p>
+          <p>
+            <strong>Last name:</strong>
+            <span>{props.lastName}</span>
+          </p>
+          <p>
+            <strong>Gender:</strong>
+            <span>{props.gender}</span>
+          </p>
+          <p>
+            <strong>Height:</strong>
+            <span>{props.height / 100}m</span>
+          </p>
+          <p>
+            <strong>Birth:</strong>
+            <span>{props.birth.toDateString()}</span>
+          </p>
+        </div>
+      </div>
+    );
+  }
